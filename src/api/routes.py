8f00257d743ba966dde184cd5dc8c1676d585eed
asolare1 <asolare1@gmail.com>
2022-08-23 01:53:27 +0000

    me

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,44 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users', methods=['GET'])
+def handle_user():  
+    store_user = User.query.all()
+    store_user = [user.serialize() for user in store_user]
+
+    return jsonify(users = store_user)
+
+
+@api.route('/users/<int:id>', methods=['GET'])
+def getsingleuser(id):  
+    store_user = User.query.filter_by(id=id).first()
+
+    return jsonify(user = store_user.serialize())
+
+
+@api.route('/people', methods=['GET'])
+def handle_people():  
+    store_people = People.query.all()
+    store_people = [people.serialize() for people in store_people]
+
+    return jsonify(people = store_people)
+
+@api.route('/people/<int:id>', methods=['GET'])
+def getsinglepeople(id):  
+    store_people = People.query.filter_by(id=id).first()
+
+    return jsonify(people = store_people.serialize())
+
+@api.route('/planets', methods=['GET'])
+def handle_planet():  
+    store_planet = Planet.query.all()
+    store_planet = [planet.serialize() for planet in store_planet]
+
+    return jsonify(planets = store_planet)
+
+@api.route('/planets/<int:id>', methods=['GET'])
+def getsingleuser(id):  
+    store_planet = Planet.query.filter_by(id=id).first()
+
+    return jsonify(planet = store_planet.serialize())
